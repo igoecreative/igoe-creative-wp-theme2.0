08c0dc801e48c64f3efadf3c3dcc8e3ec888be64
Igoe Creative <goweb@goigoecreative.com>
2018-12-07 09:16:00 -0500

    New Submit Button
    Uses Bootstrap 4 & Font Awesome instead of a HTML Character

--- searchform.php
+++ searchform.php
@@ -4,7 +4,8 @@
 		<div>
 			<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
 			<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
-			<input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( '&#128269;', 'submit button' ); ?>" />
+			<!-- <input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( '&#128269;', 'submit button' ); ?>" /> -->
+			<button class="btn search-btn my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
 		</div>
 	</form>
 </div>
