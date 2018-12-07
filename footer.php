1e2b73e4d971fcdc2f6db6297260b17eb0aaad4b
Igoe Creative <goweb@goigoecreative.com>
2018-12-07 09:39:05 -0500

    CDN Updated for Font Awesome 5.5.0

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 		<!-- /wrapper -->
 
 		<?php wp_footer(); ?>
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"  rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
 	</body>
 </html>
