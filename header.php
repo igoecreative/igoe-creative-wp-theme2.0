5e4f264e1e6be0359598d6e8f3bf2f68234cab7d
Igoe Creative <goweb@goigoecreative.com>
2018-12-07 09:44:47 -0500

    Update to Bootstrap 4
    .img-responsive to .img-fluid

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
 						<div class="row">
 							<div class="col-12 col-sm-6 logo">
 								<a href="<?php echo home_url(); ?>">
-									<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="img-responsive logo-img">
+									<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="img-fluid logo-img">
 								</a>
 							</div>
 							<div class="col-12 col-sm-6 social">
